62b0295f1bc515805dc98325aa3fdb059bb9364d
Adil Khan <adil_97@hotmail.it>
2026-02-09 14:51:57 +0000

    scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import SignOut from './components/SignOut'
+
 
 const App = () => {
-
   return (
-    <h1>Hello World!</h1>
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,9 @@
+const SignUp = () => {
+  return (
+    <main>
+      <h1>Sign Up</h1>
+    </main>
+  )
+}
+
+export default SignUp
