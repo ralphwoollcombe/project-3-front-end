8ab7df3c5cf8f8c0fca87dd7c856ef15d60eac68
ralphwoollcombe <ralph@ralphwoollcombe.com>
2026-02-09 16:29:30 +0000

    userservice.js done

---
+++ src/services/userService.js
@@ -0,0 +1,21 @@
+const BASE_URL=`${import.meta.env.VITE_BACK_END_SERVER_URL}/users`
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL, {
+        header: {authorization: `bearer ${localStorage.getItem('token')}`}
+        });
+        const data = await res.json();
+        console.log(data)
+        if (data.err) {
+            throw new Error(data.err);
+        }
+
+    return data
+    } catch (error) {
+    console.log(error);
+    throw new Error(error);
+    }
+
+
+}
