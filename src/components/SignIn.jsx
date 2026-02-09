3777081f4ccd0eaab521d7fb3385569936309c82
Adil Khan <adil_97@hotmail.it>
2026-02-09 17:24:42 +0000

    authService, SignIn and SingUp

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,20 +1,69 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { signIn } from '../services/authService'
 import { AuthContext } from '../contexts/AuthContext'
 
 const SignIn = () => {
   const navigate = useNavigate()
   const { setUser } = useContext(AuthContext)
 
-  const handleFakeSignIn = () => {
-    setUser({ username: 'adil' })
-    navigate('/')
+  const [message, setMessage] = useState('')
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (evt) => {
+    setMessage('')
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault()
+    try {
+      const signedInUser = await signIn(formData)
+      setUser(signedInUser)
+      navigate('/')
+    } catch (err) {
+      setMessage('Invalid Credentials')
+    }
   }
 
   return (
     <main>
       <h1>Sign In</h1>
-      <button onClick={handleFakeSignIn}>Fake Sign In</button>
+      <p>{message}</p>
+
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={formData.username}
+            name="username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <button type="submit">Sign In</button>
+          <button type="button" onClick={() => navigate('/')}>Cancel</button>
+        </div>
+      </form>
     </main>
   )
 }
